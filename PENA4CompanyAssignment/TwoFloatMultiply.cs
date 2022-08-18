c66cb60c0fe92d8a8959a727fda737e8be7b5ded
techinalrupali <rupa99birajdar@gmail.com>
2022-08-18 19:51:32 +0530

    First Commit

---
+++ PENA4CompanyAssignment/TwoFloatMultiply.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PENA4CompanyAssignment
+{
+    //Write a C# Program to enter two float input values and Multiply print roundup by 2 digit after decimal 
+    class TwoFloatMultiply
+    {
+        static void Main(string[] args)
+        {
+           float f1=12.55555f;
+            float f2 = 12.55555f;
+            float res = f1 * f2;
+            res = (float)Math.Round(res, 2, MidpointRounding.ToEven);
+            Console.WriteLine(res);
+        }
+
+
+
+
+
+
+    }
+}
